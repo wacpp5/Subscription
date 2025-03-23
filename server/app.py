041ed0365fc1ca6761559a4dbcffb8b58e44ef55
wacpp5 <will@teadog.com>
2025-03-23 12:25:38 -0500

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,24 @@
+# server/app.py
+from flask import Flask, request, jsonify
+from subscription_logic import get_customer_subscriptions, update_subscription
+import os
+
+app = Flask(__name__)
+
+@app.route('/subscriptions', methods=['GET'])
+def get_subscriptions():
+    customer_id = request.args.get('customer_id')
+    if not customer_id:
+        return jsonify({'error': 'Missing customer_id'}), 400
+
+    subscriptions = get_customer_subscriptions(customer_id)
+    return jsonify(subscriptions)
+
+@app.route('/subscriptions/update', methods=['POST'])
+def update_sub():
+    data = request.json
+    result = update_subscription(data)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
